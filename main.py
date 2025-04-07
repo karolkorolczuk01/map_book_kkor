d256703eb5c583d5f7d0cdcdf83691521793d4d8
karolkorolczuk01 <karol.korolczuk@student.wat.edu.pl>
2025-04-07 10:39:08 +0200

    wip: basic menu option

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from utils.model import users
 
 
 def mein():
-    get_user_info(users)
+    while True:
+        print('====================MENU======================')
+        print('0 - Exit')
+        print('1 - Get user info')
+        print('==============================================')
 
+        choice = input('Wybierz opcje menu: ')
+        if choice == '0': break
+        if choice == '1': get_user_info(users)
 
 if __name__ == '__main__':
     mein()
