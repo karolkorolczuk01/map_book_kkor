e32a74ea8cccbb103ce1d1618ed7b79ef4d55415
karolkorolczuk01 <karolkorolczuk@student.wat.edu.pl>
2025-05-21 08:45:18 +0200

    init:start empty project

--- utils/model.py
+++
@@ -1,5 +0,0 @@
-users: list[dict] = [
-    {'name': 'Karol', 'location': 'Międzyrzec Podlaski', 'posts': 500, },
-    {'name': 'Monika', 'location': 'Siedlce', 'posts': 1000, },
-    {'name': 'Kinga', 'location': 'Warszawa', 'posts': 400, }
-]
